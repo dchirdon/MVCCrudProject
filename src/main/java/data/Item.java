63b0a2343156608a9a39e65c5f6233c77226c267
invictvs <david.chirdon@gmail.com>
2017-11-19 20:10:03 -0700

    updated dependencies

--- src/main/java/data/Item.java
+++ src/main/java/data/Item.java
@@ -73,13 +73,7 @@ public String toString() {
 	return "Item [name=" + type + ", id=" + id + ", value=" + value + "]";
 }
 
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + id;
-	return result;
-}
+
 
 @Override
 public boolean equals(Object obj) {
